e30beb9431167265c74316648b6aa4a29fd7f4c1
Vlad <blackmor143@yandex.ru>
2022-11-20 23:55:42 +0300

    Выполнил 1 задание

---
+++ Zadanie_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+double[] GetArrayOfNumbers()
+{
+    Console.WriteLine("Введите 3 числа через пробел: ");
+    string UserMassive = Console.ReadLine();
+
+    double[] numbers = UserMassive.Split(' ').Select(double.Parse).ToArray();
+  
+    return numbers;
+}
+
+int CountPositiveNumbers(double[] array)
+{
+    int posNumbers = 0;
+    foreach (double el in array)
+    {
+        if (el > 0) posNumbers++;
+    }
+    Console.WriteLine($"Положительных чисел {posNumbers}.");
+    return posNumbers;
+}
+
+Console.WriteLine();
+CountPositiveNumbers(GetArrayOfNumbers());
